0efff223e3061145c08a2091da70e04a3ac61a6b
Hans Foster <fosterhans@gmail.com>
2019-09-23 03:17:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
-  count = array.length
+  count = 0
   square_array_return = []
-  while count > 0
+  while count < array.length
     square_array_return.push(array[count] * array[count])
 end
